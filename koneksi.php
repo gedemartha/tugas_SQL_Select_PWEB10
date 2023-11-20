438d42dd88c82a9b96b196fd4dd4d7a6c890c811
Martha <87524124+gedemartha@users.noreply.github.com>
2023-11-20 09:53:09 +0800

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "2101020020_martha";
+
+    $con = mysqli_connect($server,$user,$password,$database);
+
+
+?>
